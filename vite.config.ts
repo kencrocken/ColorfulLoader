81b437d48cd3df8772f0b08dcc60a50b6e7e66e1
Ken Crocken <kcrocken@gmail.com>
2024-06-04 14:34:12 -0400

    chore: Add workflow to publish story book to gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -9,18 +9,18 @@ export default defineConfig({
       entry: './src/index.ts', // Specifies the entry point for building the library.
       name: 'vite-react-ts-button', // Sets the name of the generated library.
       fileName: format => `index.${format}.js`, // Generates the output file name based on the format.
-      formats: ['cjs', 'es'], // Specifies the output formats (CommonJS and ES modules).
+      formats: ['cjs', 'es'] // Specifies the output formats (CommonJS and ES modules).
     },
     rollupOptions: {
-      external: [...Object.keys(peerDependencies)], // Defines external dependencies for Rollup bundling.
+      external: [...Object.keys(peerDependencies)] // Defines external dependencies for Rollup bundling.
     },
     sourcemap: true, // Generates source maps for debugging.
-    emptyOutDir: true, // Clears the output directory before building.
+    emptyOutDir: true // Clears the output directory before building.
   },
   plugins: [dts()], // Uses the 'vite-plugin-dts' plugin for generating TypeScript declaration files (d.ts).
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './setupTests.ts',
-  },
+    setupFiles: './setupTests.ts'
+  }
 });
